3d321e33e23d309aac851a46feb0baccc1d5141a
Truong Huu Nam <truonghuunam2002@gmail.com>
2024-11-11 15:44:09 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Git</title>
+</head>
+<body>
+    <?php 
+        phpinfo();
+        $a = array("SV1" => "HuuNam","SV2" => "AnhTuan");
+        print_r($a);
+    ?>  
+</body>
+</html>
